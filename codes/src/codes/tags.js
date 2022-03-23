bfe754dd843ae76d7fcd22825ff6ee17c68effe7
nmn <nmngarg030798@gmail.com>
2022-03-23 14:14:00 +0530

    code-refactored

--- codes/src/codes/tags.js
+++ codes/src/codes/tags.js
@@ -1,34 +1,19 @@
 import React from 'react';
-import got from './data.js'
-import Card from './cards.js'
+
 // console.log(got);
 
 
-
-class Tags extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            nameTag: "",
-        }
-    }
-    handleClick(name){
-        this.setState({
-            nameTag:name,
-        })
-    }
-    render(){
-        return (
-            <>
-            <ul className="all-house-names">
-                {
-                    got.houses.map(house => <li key={house.name} className={(this.state.nameTag === house.name) ? 'active' : ''} onClick={() => this.handleClick(house.name)} >{house.name}</li>)
-                }
-            </ul>
-            <Card allPeople={(!this.state.nameTag) ? got.houses.map(house => house.people) : got.houses.find(house => (house.name === this.state.nameTag)).people}/>
-            </>
-        )
-    }
+function Tags(props){
+    return (
+        <>
+        <ul className="all-house-names">
+            {
+                props.data.map(house => <li key={house.name} className={(props.nameTag === house.name) ? 'active' : ''} onClick={() => props.handleClick(house.name)} >{house.name}</li>)
+            }
+        </ul>
+        
+        </>
+    )
 }
 
 export default Tags;
--- codes/src/index.js
+++ codes/src/index.js
@@ -1,16 +1,32 @@
-import styles from './stylesheets/style.css';
+import  './stylesheets/style.css';
 import React from 'react';
+import got from './codes/data.js'
 import ReactDOM from 'react-dom';
 import Tags from './codes/tags'
+import Card from './codes/cards.js'
 
 
 
-function App (){
-    return (
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            nameTag: "",
+        }
+    }
+    handleClick(name){
+        this.setState({
+            nameTag:name,
+        })
+    }
+    render () {
+        return (
         <>
-            <Tags />
+            <Tags data={got.houses} nameTag={this.state.nameTag} handleClick={ (value) =>  this.handleClick(value)} />
+            <Card allPeople={(!this.state.nameTag) ? got.houses.map(house => house.people) : got.houses.find(house => (house.name === this.state.nameTag)).people}/>
         </>
-    )
+        )
+        }
 }
 
 ReactDOM.render(<App />,document.getElementById(`parent-of-ul`));
